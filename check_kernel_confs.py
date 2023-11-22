d0482a6e580d3252ed124645907dc7dade03f3f2
ramin <lordrasmus@gmail.com>
2023-11-22 09:28:49 +0100

    config check script

---
+++ check_kernel_confs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+
+import glob
+
+confs = glob.glob("*.config")
+
+
+checks= ["CONFIG_SIGNALFD", "CONFIG_TIMERFD","CONFIG_SYSVIPC","CONFIG_POSIX_MQUEUE"]
+
+for c in confs:
+    with open(c,"r") as f:
+        data = f.read()
+        
+
+    print( c )
+    
+    for ch in checks:
+        if not ch+"=y" in data:
+            print( "   \033[01;32m" + ch + "\033[00m aktivieren" )
+
+
